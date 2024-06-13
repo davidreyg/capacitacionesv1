afbb580427072f968a0510decd305700906ae51d
David Rey <neisserrey@upeu.edu.pe>
2024-06-12 22:43:42 -0500

    feat: :boom: docker!

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import laravel, { refreshPaths } from 'laravel-vite-plugin'
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'],
-            refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/filament/admin/theme.css",
             ],
+            refresh: [...refreshPaths, "app/Livewire/**"],
         }),
     ],
-})
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+    },
+});
