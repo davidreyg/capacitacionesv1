b4cd77585e3871c2f8fc1b4bfe5b578ac7c4f9a6
David Rey <neisserrey@upeu.edu.pe>
2024-07-10 16:40:52 -0500

    feat: :boom: Editar evento - Pagina
    * Cambiamos el titulo a reprogramar
    * Al cargar la pagina llena las solicitudes asociadas
    * al guardar usa la fecha de hoy y el usaurio atutenticado para la reprogramacion
    * Al guardar por cada solicitud seleccionada transiciona de estado a Aprobado. en caso de que se pueda.

--- app/Filament/Resources/EventoResource/Pages/EditEvento.php
+++ app/Filament/Resources/EventoResource/Pages/EditEvento.php
@@ -3,12 +3,48 @@
 namespace App\Filament\Resources\EventoResource\Pages;
 
 use App\Filament\Resources\EventoResource;
+use App\Models\Solicitud;
+use App\States\Solicitud\Aprobado;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditEvento extends EditRecord
 {
+
+    protected ?string $heading = 'Reprogramar Evento';
     protected static string $resource = EventoResource::class;
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data['solicitud_ids'] = $this->getRecord()->solicituds()->pluck('id');
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $data['fecha_reprogramacion'] = now();
+        $data['reprogramador_id'] = auth()->id();
+        return $data;
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $record->update($data);
+        foreach ($data['solicitud_ids'] as $value) {
+            $solcitud = Solicitud::find($value);
+            $solcitud->evento_id = $record->id;
+            $solcitud->save();
+            if ($solcitud->estado->canTransitionTo(Aprobado::class)) {
+                $solcitud->estado->transitionTo(Aprobado::class);
+            }
+        }
+        return $record;
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->previousUrl ?? $this->getResource()::getUrl('index');
+    }
 
     protected function getHeaderActions(): array
     {
