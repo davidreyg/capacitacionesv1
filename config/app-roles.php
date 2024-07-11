3ba09d54474b82390c17e81f4a5a88e371578f8a
David Rey <neisserrey@upeu.edu.pe>
2024-07-11 17:36:42 -0500

    feat: :sparkles: Configuramos roles por defecto para nuestra app.

---
+++ config/app-roles.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | AppSection Section Establecimiento Container
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    */
+    "roles" => [
+        // "super_admin" => "esta definido en el archivo filament-shield.php",
+        "diris" => "diris",
+        "ris" => "ris",
+        "establecimiento" => "establecimiento",
+    ],
+
+];
