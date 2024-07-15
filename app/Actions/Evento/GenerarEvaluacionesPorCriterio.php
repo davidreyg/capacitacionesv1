224e64e05a8dc846fe96a9414373829ee4f5063b
David Rey <neisserrey@upeu.edu.pe>
2024-07-15 16:54:37 -0500

    refactor: :boom: Mejoramos la logica de esta clase
    * Ahora aceptamos como parametro el criterio para e lcual deseamso generar las evaluaciones.

--- app/Actions/Evento/GenerarEvaluacionesPorCriterio.php
+++ app/Actions/Evento/GenerarEvaluacionesPorCriterio.php
@@ -7,20 +7,11 @@ use Filament\Notifications\Notification;
 use Filament\Support\Exceptions\Halt;
 use Lorisleiva\Actions\Concerns\AsAction;
 
-class GenerarEvaluaciones
+class GenerarEvaluacionesPorCriterio
 {
     use AsAction;
-    public function handle(Evento $evento): void
+    public function handle(Evento $evento, int $criterioId): void
     {
-        // Obtener los criterios de evaluación para el evento
-        $criterios = $evento->criterioEvaluacions;
-        if ($criterios->count() === 0) {
-            Notification::make()
-                ->title("No ha configurado sus criterios de evaluacion.")
-                ->danger()
-                ->send();
-            throw new Halt("No ha configurado sus criterios de evaluacion");
-        }
         // Obtener todos los alumnos
         $empleados = $evento->empleados;
         if ($empleados->count() === 0) {
@@ -36,7 +27,7 @@ class GenerarEvaluaciones
                 [
                     'evento_id' => $evento->id,
                     'empleado_id' => $empleado->id,
-                    'criterio_evaluacion_id' => $criterios->first()->id
+                    'criterio_evaluacion_id' => $criterioId
                 ],
                 [
                     'nota' => 0 // Valor inicial, ajustar según necesidad
