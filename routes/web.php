2b1fb464ff3f66cfb93800fd5fa5c87361b13781
David Rey <neisserrey@upeu.edu.pe>
2024-07-16 16:33:36 -0500

    chore: :heavy_plus_sign: browsershot probando

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use Spatie\Browsershot\Browsershot;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/test', function () {
+    $html = view('welcome')->render();
+
+    $pdf_content = Browsershot::html($html)
+        ->timeout(60)
+        ->waitUntilNetworkIdle()
+        ->windowSize(502, 850)
+        ->noSandbox();
+    if (config('app-pdf.node')) {
+        $pdf_content = $pdf_content->setNodeBinary(config('app-pdf.node'));
+    }
+
+    if (config('app-pdf.npm')) {
+        $pdf_content = $pdf_content->setNpmBinary(config('app-pdf.npm'));
+    }
+
+    if (config('app-pdf.modules')) {
+        $pdf_content = $pdf_content->setIncludePath(config('app-pdf.modules'));
+    }
+
+    if (config('app-pdf.chrome')) {
+        $pdf_content = $pdf_content->setChromePath(config('app-pdf.chrome'));
+    }
+
+    if (config('app-pdf.chrome_arguments')) {
+        $pdf_content = $pdf_content->addChromiumArguments(config('app-pdf.chrome_arguments'));
+    }
+
+    return new Response($pdf_content->pdf(), 200, [
+        'Content-Type' => 'application/pdf',
+        'Content-Disposition' => 'inline; filename="example.pdf"'
+    ]);
+});
