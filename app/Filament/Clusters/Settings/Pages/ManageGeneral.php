7315a825e74119e6f5d90148e33ea8c47a8c3050
David Rey <neisserrey@upeu.edu.pe>
2024-07-18 23:03:19 -0500

    fix: :recycle: eliminamos el aceeditor y los colores los movemos a appearance

--- app/Filament/Clusters/Settings/Pages/ManageGeneral.php
+++ app/Filament/Clusters/Settings/Pages/ManageGeneral.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Clusters\Settings\Pages;
 
 use App\Filament\Clusters\Settings;
-use App\Services\FileService;
 use App\Settings\GeneralSettings;
 use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use Filament\Forms;
@@ -12,8 +11,6 @@ use Filament\Notifications\Notification;
 use Filament\Pages\SettingsPage;
 use Filament\Support\Facades\FilamentView;
 use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Support\Facades\Storage;
-use Riodwanto\FilamentAceEditor\AceEditor;
 
 use function Filament\Support\is_app_url;
 
@@ -37,24 +34,13 @@ class ManageGeneral extends SettingsPage
 
     public function mount(): void
     {
-        $this->themePath = resource_path('css/filament/admin/theme.css');
-        $this->twConfigPath = resource_path('css/filament/admin/tailwind.config.js');
-
         $this->fillForm();
     }
 
     protected function fillForm(): void
     {
         $settings = app(static::getSettings());
-
         $data = $this->mutateFormDataBeforeFill($settings->toArray());
-
-        $fileService = new FileService;
-
-        $data['theme-editor'] = $fileService->readfile($this->themePath);
-
-        $data['tw-config-editor'] = $fileService->readfile($this->twConfigPath);
-
         $this->form->fill($data);
     }
 
@@ -106,41 +92,6 @@ class ManageGeneral extends SettingsPage
                                 ->required(),
                         ])->columns(4),
                     ]),
-                Forms\Components\Tabs::make('Tabs')
-                    ->tabs([
-                        Forms\Components\Tabs\Tab::make('Color Palette')
-                            ->schema([
-                                Forms\Components\ColorPicker::make('site_theme.primary')
-                                    ->label(fn() => __('page.general_settings.fields.primary'))->rgb(),
-                                Forms\Components\ColorPicker::make('site_theme.secondary')
-                                    ->label(fn() => __('page.general_settings.fields.secondary'))->rgb(),
-                                // Forms\Components\ColorPicker::make('site_theme.gray')
-                                //     ->label(fn() => __('page.general_settings.fields.gray'))->rgb(),
-                                Forms\Components\ColorPicker::make('site_theme.success')
-                                    ->label(fn() => __('page.general_settings.fields.success'))->rgb(),
-                                Forms\Components\ColorPicker::make('site_theme.danger')
-                                    ->label(fn() => __('page.general_settings.fields.danger'))->rgb(),
-                                Forms\Components\ColorPicker::make('site_theme.info')
-                                    ->label(fn() => __('page.general_settings.fields.info'))->rgb(),
-                                Forms\Components\ColorPicker::make('site_theme.warning')
-                                    ->label(fn() => __('page.general_settings.fields.warning'))->rgb(),
-                            ])
-                            ->columns(3),
-                        Forms\Components\Tabs\Tab::make('Code Editor')
-                            ->schema([
-                                Forms\Components\Grid::make()->schema([
-                                    AceEditor::make('theme-editor')
-                                        ->label('theme.css')
-                                        ->mode('css')
-                                        ->height('24rem'),
-                                    AceEditor::make('tw-config-editor')
-                                        ->label('tailwind.config.js')
-                                        ->height('24rem')
-                                ])
-                            ]),
-                    ])
-                    ->persistTabInQueryString()
-                    ->columnSpanFull(),
             ])
             ->columns(3)
             ->statePath('data');
@@ -155,10 +106,6 @@ class ManageGeneral extends SettingsPage
 
             $settings->fill($data);
             $settings->save();
-
-            $fileService = new FileService;
-            $fileService->writeFile($this->themePath, $data['theme-editor']);
-            $fileService->writeFile($this->twConfigPath, $data['tw-config-editor']);
 
             Notification::make()
                 ->title('Settings updated.')
