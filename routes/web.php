6e0c6f8fcd81eee7aab7738a27ecb7efd08df315
David Rey <neisserrey@upeu.edu.pe>
2024-07-21 18:53:58 +0000

    fix: :boom: Miramos la logica de generar pdf por url a un contenido html para eso usamos el Action de generarpdf

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Livewire\ReporteAsistencia;
-use Illuminate\Http\Response;
+use App\Actions\GenerarPdf;
+use App\Enums\Setting\ReportType;
 use Illuminate\Support\Facades\Route;
-use Spatie\Browsershot\Browsershot;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,45 +14,7 @@ use Spatie\Browsershot\Browsershot;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/asistencia', ReporteAsistencia::class)->name('asistencia')->middleware(['force.https']);
-Route::get('/test', function () {
-    $html = view('welcome')->render();
-    $route = route('asistencia', absolute: false);
-    // Obtener la IP del contenedor usando gethostbyname()
-    $containerIp = gethostbyname('laravel.test');
-    // Construir la URL completa con la IP del contenedor
-    $url = "http://laravel.test{$route}";
-    $pdf_content = Browsershot::url($url)
-        ->timeout(50)
-        ->ignoreHttpsErrors()
-        ->format('A4')
-        ->setOption('newHeadless', true)
-        ->waitUntilNetworkIdle()
-        ->showBackground()
-        ->noSandbox()
-        ->noSandbox();
-    if (config('app-pdf.node')) {
-        $pdf_content = $pdf_content->setNodeBinary(config('app-pdf.node'));
-    }
-
-    if (config('app-pdf.npm')) {
-        $pdf_content = $pdf_content->setNpmBinary(config('app-pdf.npm'));
-    }
-
-    if (config('app-pdf.modules')) {
-        $pdf_content = $pdf_content->setIncludePath(config('app-pdf.modules'));
-    }
-
-    if (config('app-pdf.chrome')) {
-        $pdf_content = $pdf_content->setChromePath(config('app-pdf.chrome'));
-    }
-
-    if (config('app-pdf.chrome_arguments')) {
-        $pdf_content = $pdf_content->addChromiumArguments(config('app-pdf.chrome_arguments'));
-    }
-
-    return new Response($pdf_content->pdf(), 200, [
-        'Content-Type' => 'application/pdf',
-        'Content-Disposition' => 'inline; filename="example.pdf"'
-    ]);
+// Route::get('/asistencia', ReporteAsistencia::class)->name('asistencia')->middleware(['force.https']);
+Route::get('/preview-pdf', function () {
+    return GenerarPdf::make()->handle(ReportType::ASISTENCIA, ['nombre' => 'david', 'perro' => 'loco gallo']);
 });
