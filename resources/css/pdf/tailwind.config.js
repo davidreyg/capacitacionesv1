de7062437fc1abcab1cffd1cb367a049f5fbe3ed
David Rey <neisserrey@upeu.edu.pe>
2024-07-23 01:31:38 +0000

    fix: :bug: modificamos el content del config de tailwind para el hot reload

--- resources/css/pdf/tailwind.config.js
+++ resources/css/pdf/tailwind.config.js
@@ -1,18 +1,8 @@
-import defaultTheme from "tailwindcss/defaultTheme";
 
-/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./resources/views/components/layouts/pdf.blade.php",
-        "./resources/views/components/pdf/**.blade.php",
-        "./resources/views/livewire/**/*.blade.php",
+        "./resources/views/components/pdf/*.blade.php",
+        "./resources/views/components/reporte-asistencia.blade.php",
     ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
 };
