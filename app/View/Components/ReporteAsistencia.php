5ca061de1947b6e256ce098b896ec87a41165a02
David Rey <neisserrey@upeu.edu.pe>
2024-07-23 01:35:45 +0000

    feat: :sparkles: Enviamos la fuente escogida al reporte para ser usado

--- app/View/Components/ReporteAsistencia.php
+++ app/View/Components/ReporteAsistencia.php
@@ -6,18 +6,26 @@ use App\DTO\AsistenciaData;
 use App\Settings\GeneralSettings;
 use App\Settings\ReportSettings;
 use Closure;
+use Filament\Panel\Concerns\HasFont;
+use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class ReporteAsistencia extends Component
 {
+    use HasFont;
     public ReportSettings $reportSettings;
     public GeneralSettings $generalSettings;
     public AsistenciaData $datos;
+    public string $fontFam;
+    public Htmlable $fontHtml;
 
     public function __construct(AsistenciaData $datos, ReportSettings $reportSettings, GeneralSettings $generalSettings)
     {
         $this->reportSettings = $reportSettings;
+        $this->fontFam = $this->reportSettings->font->getLabel();
+        $this->fontHtml = $this->font($this->reportSettings->font->getLabel())->getFontHtml();
+
         $this->generalSettings = $generalSettings;
         $this->datos = $datos;
     }
