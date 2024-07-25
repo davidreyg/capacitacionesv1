5f84dbae5bbda1ffa353d242f125c98dd9392ba4
David Rey <neisserrey@upeu.edu.pe>
2024-07-25 05:06:34 +0000

    feat: :sparkles: Trait para saber que usuario esta loggeado

---
+++ app/Traits/CheckUserType.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Traits;
+
+use App\Models\User;
+use Filament\Facades\Filament;
+
+trait CheckUserType
+{
+    public function isEmpleado(): bool
+    {
+        return $this->getUser()?->empleado !== null;
+    }
+    public function isProvedor(): bool
+    {
+        return $this->getUser()?->proveedor !== null;
+    }
+
+    public function getUser(): User|null
+    {
+        return Filament::auth()->check() ? Filament::auth()->user() : null;
+    }
+}
