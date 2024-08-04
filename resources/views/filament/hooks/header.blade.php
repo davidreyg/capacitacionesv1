aa74bcf251c6ab14d6ec793277378e532e5ad07d
David Rey <neisserrey@upeu.edu.pe>
2024-08-04 00:23:06 +0000

    fix: :ambulance: si es proveedor solo mostramos su rzaon social

--- resources/views/filament/hooks/header.blade.php
+++ resources/views/filament/hooks/header.blade.php
@@ -1,24 +1,36 @@
 <div>
     <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
     @auth
-        <div class="flex justify-center gap-x-4 rounded-lg">
-            <span class="flex items-center gap-x-2">
-                <span class="text-sm font-semibold px-2.5 rounded">
-                    Código:
+        @if (auth()->user()->isEmpleado())
+            <div class="flex justify-center gap-x-4 rounded-lg">
+                <span class="flex items-center gap-x-2">
+                    <span class="text-sm font-semibold px-2.5 rounded">
+                        Código:
+                    </span>
+                    <x-filament::badge color="success">
+                        <span class="font-extrabold text-lg"> {{ auth()->user()->empleado->establecimiento->codigo }}</span>
+                    </x-filament::badge>
                 </span>
-                <x-filament::badge color="success">
-                    <span class="font-extrabold text-lg"> {{ auth()->user()->empleado->establecimiento->codigo }}</span>
-                </x-filament::badge>
-            </span>
-            <span class="flex items-center gap-x-2">
-                <span class="text-sm font-semibold px-2.5 rounded">
-                    Entidad:
+                <span class="flex items-center gap-x-2">
+                    <span class="text-sm font-semibold px-2.5 rounded">
+                        Entidad:
+                    </span>
+                    <x-filament::badge color="info" size="lg">
+                        <span class="font-extrabold text-lg">{{ auth()->user()->empleado->establecimiento->nombre }}</span>
+                    </x-filament::badge>
                 </span>
-                <x-filament::badge color="info" size="lg">
-                    <span class="font-extrabold text-lg">{{ auth()->user()->empleado->establecimiento->nombre }}</span>
-                </x-filament::badge>
-            </span>
-        </div>
-
+            </div>
+        @else
+            <div class="flex justify-center gap-x-4 rounded-lg">
+                <span class="flex items-center gap-x-2">
+                    <span class="text-sm font-semibold px-2.5 rounded">
+                        Proveedor:
+                    </span>
+                    <x-filament::badge color="success">
+                        <span class="font-extrabold text-lg"> {{ auth()->user()->proveedor->razon_social }}</span>
+                    </x-filament::badge>
+                </span>
+            </div>
+        @endif
     @endauth
 </div>
