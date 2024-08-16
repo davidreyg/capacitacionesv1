c90693720336822cc4731abf124505a07240efe7
David Rey <neisserrey@upeu.edu.pe>
2024-08-16 05:39:09 +0000

    fix: :ambulance: Mejoramos el dashboard del  admin

--- app/Filament/Admin/Widgets/StatsAdminOverview.php
+++ app/Filament/Admin/Widgets/StatsAdminOverview.php
@@ -5,8 +5,13 @@ namespace App\Filament\Admin\Widgets;
 use App\Models\Empleado;
 use App\Models\Evento;
 use App\Models\Solicitud;
+use App\States\Evento\Evaluado;
+use App\States\Evento\Finalizado;
+use App\States\Solicitud\Aprobado;
+use App\States\Solicitud\Solicitado;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Database\Eloquent\Builder;
 
 // FIXME: Falta arreglar esto.
 class StatsAdminOverview extends BaseWidget
@@ -16,15 +21,28 @@ class StatsAdminOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('N° de empleados', Empleado::count())
-                ->description('32k increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('success'),
             Stat::make('N° de Programaciones', Evento::count())
                 // ->description('32k increase')
                 // ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('info'),
-            Stat::make('N° de Solicitudes', Solicitud::count())
+
+            Stat::make('N° de empleados', Empleado::count())
+                ->color('success'),
+            Stat::make('N° de empleados capacitados', Empleado::whereHas('eventos')->count())
+                ->color('success'),
+
+
+            Stat::make('N° cursos solicitados', Solicitud::count())
+                ->description('')
+                ->descriptionIcon('tabler-alert-circle')
+                ->color('info'),
+            Stat::make('N° cursos aprobados', Solicitud::whereNotState('estado', [Solicitado::class, Evaluado::class])->count())
+                ->description('')
+                ->descriptionIcon('tabler-alert-circle')
+                ->color('info'),
+            Stat::make('N° cursos finalizados', Solicitud::whereHas('evento', function (Builder $query) {
+                $query->whereState('estado', Finalizado::class);
+            })->count())
                 ->description('')
                 ->descriptionIcon('tabler-alert-circle')
                 ->color('info'),
