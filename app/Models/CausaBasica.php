9bfddd359ae9fc85d2429bd1756bee0222c09268
David Rey <neisserrey@upeu.edu.pe>
2024-08-20 14:35:31 +0000

    fix: :ambulance: Causa Basica recursivo y agregamos relacion con nacs

--- app/Models/CausaBasica.php
+++ app/Models/CausaBasica.php
@@ -7,7 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class CausaBasica extends Model
 {
+    use \Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
     use HasFactory;
     public $timestamps = false;
     protected $fillable = ['nombre', 'parent_id'];
+
+    public function nacs()
+    {
+        return $this->belongsToMany(Nac::class, 'causa_basica_nac');
+    }
 }
