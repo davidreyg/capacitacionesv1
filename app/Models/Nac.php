544f3836205c349fbee9eb26d2fdf88e2f990234
David Rey <neisserrey@upeu.edu.pe>
2024-08-20 19:16:58 +0000

    fix: :adhesive_bandage: Agregamos recursividad al modelo Nac

--- app/Models/Nac.php
+++ app/Models/Nac.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nac extends Model
 {
+    use \Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = ['nombre', 'parent_id'];
     use HasFactory;
 }
