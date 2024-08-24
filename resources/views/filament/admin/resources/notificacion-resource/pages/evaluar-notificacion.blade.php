f1625afaf2c3f1494e9acc13d6d30bd66f399779
David Rey <neisserrey@upeu.edu.pe>
2024-08-24 19:26:08 +0000

    fix: :lipstick:Usamos clases del resource

--- resources/views/filament/admin/resources/notificacion-resource/pages/evaluar-notificacion.blade.php
+++ resources/views/filament/admin/resources/notificacion-resource/pages/evaluar-notificacion.blade.php
@@ -1,5 +1,10 @@
-<x-filament-panels::page>
-    <x-filament-panels::form wire:submit="save">
+<x-filament-panels::page @class([
+    'fi-resource-edit-record-page',
+    'fi-resource-' . str_replace('/', '-', $this->getResource()::getSlug()),
+    'fi-resource-record-' . $record->getKey(),
+])>
+    <x-filament-panels::form id="form" :wire:key="$this->getId() . '.forms.' . $this->getFormStatePath()"
+        wire:submit="save">
         {{ $this->form }}
 
         <x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" />
