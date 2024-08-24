6543f091c5f1ec307c054fc389d7131f69e09f79
David Rey <neisserrey@upeu.edu.pe>
2024-08-24 19:26:38 +0000

    fix: :lipstick: Relacion tipoContactos en causainmediata

--- app/Models/CausaInmediata.php
+++ app/Models/CausaInmediata.php
@@ -11,6 +11,11 @@ class CausaInmediata extends Model
     public $timestamps = false;
     protected $fillable = ['nombre'];
 
+    public function tipoContactos()
+    {
+        return $this->belongsToMany(TipoContacto::class, 'tipo_contacto_causa_inmediata');
+    }
+
     public function causaBasicas()
     {
         return $this->belongsToMany(CausaBasica::class, 'causa_inmediata_causa_basica');
